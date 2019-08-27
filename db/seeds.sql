f7ad0255fb7ce9193a97ac8ce08f4c4c3c960dd6
NelsonCad <NelsonCad97@gmail.com>
2019-08-27 13:53:54 -0700

    base files have been added with variable name changes to some files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Double Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name, consumed) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (name, consumed) VALUES ('Animal Style Cheese Burger', true);
+INSERT INTO burgers (name, consumed) VALUES ('4x4 Cheeses Burger', true);
+INSERT INTO burgers (name) VALUES ('Chicken Burger');
